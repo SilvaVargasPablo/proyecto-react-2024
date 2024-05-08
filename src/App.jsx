fdea9dd73819c01a692a9ef484dd60c40302d3bf
SilvaVargasPablo <73146647+SilvaVargasPablo@users.noreply.github.com>
2024-05-08 16:09:37 -0400

    segundo-commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import { useEffect } from "react"
 function App() {
 
   return (
-    useEffect(() =>{}, [])
+    useEffect(() =>{
+      fetch("api.coincap.io/v2/assets")
+        .then((resp)  => resp.jason())
+        .then((resp) => {
+          console.log(data)
+        })
+        .catch(() => {
+          console.error("la peticion fallo")
+        })
+    }, [])
     <h1>Lista de criptomonedas</h1>
   )
 }
