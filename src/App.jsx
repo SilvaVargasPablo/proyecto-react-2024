c4e374cdf06d09045402719fd8990a4c551dae4b
SilvaVargasPablo <73146647+SilvaVargasPablo@users.noreply.github.com>
2024-05-14 16:03:21 -0400

    3er commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
+import { useState } from "react"
 import { useEffect } from "react"
 
 function App() {
 
+  const [criptos, setCriptos] = useState([]) 
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCriptos(data.data)
+      })
+      .catch(() =>{
+        console.error("Lapeticion falló")
+      })
+    
+  }, [])
+
   return (
-    useEffect(() =>{
-      fetch("api.coincap.io/v2/assets")
-        .then((resp)  => resp.jason())
-        .then((resp) => {
-          console.log(data)
-        })
-        .catch(() => {
-          console.error("la peticion fallo")
-        })
-    }, [])
-    <h1>Lista de criptomonedas</h1>
+    <>
+      <h1> Lista de criptomonedas </h1>
+      <ol>
+        { criptos.map(({name, priceUsd}) => ( 
+         <li>Name:{name} Price:{priceUsd}</li>
+        )) }
+      </ol>
+    </>
   )
 }
 
